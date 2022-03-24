ea180be56f3b1cf375d033bdaaafeb8731ecca63
Breno Pereira <brenopereira856@gmail.com>
2022-03-24 16:34:14 -0300

    Initial Commit

---
+++ vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-if="Auth == true">
+      <Authentication></Authentication>
+    </div>
+  </div>
+</template>
+
+<script>
+import Authentication from './components/Authentication.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      Auth: true
+    }
+  },
+  components: {
+    Authentication
+  }
+}
+</script>
---
+++ vue/src/components/Authentication.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="AuthComp">
+    <div class="authSidebar">
+      <h2>Entrar</h2>
+      <p>Bem-vindo de volta ao TeleViewCare.</p>
+
+      <div class="inputSection">
+        <label for="u_email"><i class="fa-solid fa-envelope"></i></label>
+        <input id="u_email" type="text" placeholder="E-mail">
+      </div>
+      <div class="inputSection">
+        <label for="u_pass"><i class="fa-solid fa-key"></i></label>
+        <input id="u_pass" type="password" placeholder="Senha">
+      </div>
+      <div>
+        <button class="primary">Entrar</button>
+        <p>Não possui conta? <button class="empty">Registre-se</button>.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AuthComp'
+}
+</script>
